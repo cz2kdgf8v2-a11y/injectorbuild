76630c05d19fb62645be6e3623ca77a7c6a519fc
cz2kdgf8v2-a11y <cz2kdgf8v2@privaterelay.appleid.com>
2026-09-21 19:02:40 +0200

    Update syscalls.h

--- syscalls.h
+++ syscalls.h
@@ -1,10 +1,7 @@
 #pragma once
 
-// Code below is adapted from @modexpblog. Read linked article for more details.
-// https://www.mdsec.co.uk/2020/12/bypassing-user-mode-hooks-and-direct-invocation-of-system-calls-for-red-teams
-
-#ifndef SW3_HEADER_H_
-#define SW3_HEADER_H_
+#ifndef QN3_HEADER_H_
+#define QN3_HEADER_H_
 
 #include <windows.h>
 
@@ -13,139 +10,106 @@ typedef _Return_type_success_(return >= 0) LONG NTSTATUS;
 typedef NTSTATUS* PNTSTATUS;
 #endif
 
-#define SW3_SEED 0x757469BF
-#define SW3_ROL8(v) (v << 8 | v >> 24)
-#define SW3_ROR8(v) (v >> 8 | v << 24)
-#define SW3_ROX8(v) ((SW3_SEED % 2) ? SW3_ROL8(v) : SW3_ROR8(v))
-#define SW3_MAX_ENTRIES 600
-#define SW3_RVA2VA(Type, DllBase, Rva) (Type)((ULONG_PTR) DllBase + Rva)
+#define QN3_SEED        0x757469BF
+#define QN3_ROL8(v)     (v << 8 | v >> 24)
+#define QN3_ROR8(v)     (v >> 8 | v << 24)
+#define QN3_MAX_ENTRIES 600
+#define QN3_RVA2VA(T, B, R) (T)((ULONG_PTR)(B) + (R))
 
-// Typedefs are prefixed to avoid pollution.
-
-typedef struct _SW3_SYSCALL_ENTRY
-{
+typedef struct _QN3_SYSCALL_ENTRY {
     DWORD Hash;
     DWORD Address;
-	PVOID SyscallAddress;
-} SW3_SYSCALL_ENTRY, *PSW3_SYSCALL_ENTRY;
+    PVOID SyscallAddress;
+} QN3_SYSCALL_ENTRY, *PQN3_SYSCALL_ENTRY;
 
-typedef struct _SW3_SYSCALL_LIST
-{
+typedef struct _QN3_SYSCALL_LIST {
     DWORD Count;
-    SW3_SYSCALL_ENTRY Entries[SW3_MAX_ENTRIES];
-} SW3_SYSCALL_LIST, *PSW3_SYSCALL_LIST;
+    QN3_SYSCALL_ENTRY Entries[QN3_MAX_ENTRIES];
+} QN3_SYSCALL_LIST, *PQN3_SYSCALL_LIST;
 
-typedef struct _SW3_PEB_LDR_DATA {
-	BYTE Reserved1[8];
-	PVOID Reserved2[3];
-	LIST_ENTRY InMemoryOrderModuleList;
-} SW3_PEB_LDR_DATA, *PSW3_PEB_LDR_DATA;
+typedef struct _QN3_PEB_LDR_DATA {
+    BYTE  Reserved1[8];
+    PVOID Reserved2[3];
+    LIST_ENTRY InMemoryOrderModuleList;
+} QN3_PEB_LDR_DATA, *PQN3_PEB_LDR_DATA;
 
-typedef struct _SW3_LDR_DATA_TABLE_ENTRY {
-	PVOID Reserved1[2];
-	LIST_ENTRY InMemoryOrderLinks;
-	PVOID Reserved2[2];
-	PVOID DllBase;
-} SW3_LDR_DATA_TABLE_ENTRY, *PSW3_LDR_DATA_TABLE_ENTRY;
+typedef struct _QN3_LDR_DATA_TABLE_ENTRY {
+    PVOID Reserved1[2];
+    LIST_ENTRY InMemoryOrderLinks;
+    PVOID Reserved2[2];
+    PVOID DllBase;
+} QN3_LDR_DATA_TABLE_ENTRY, *PQN3_LDR_DATA_TABLE_ENTRY;
 
-typedef struct _SW3_PEB {
-	BYTE Reserved1[2];
-	BYTE BeingDebugged;
-	BYTE Reserved2[1];
-	PVOID Reserved3[2];
-	PSW3_PEB_LDR_DATA Ldr;
-} SW3_PEB, *PSW3_PEB;
+typedef struct _QN3_PEB {
+    BYTE  Reserved1[2];
+    BYTE  BeingDebugged;
+    BYTE  Reserved2[1];
+    PVOID Reserved3[2];
+    PQN3_PEB_LDR_DATA Ldr;
+} QN3_PEB, *PQN3_PEB;
 
-DWORD SW3_HashSyscall(PCSTR FunctionName);
-BOOL SW3_PopulateSyscallList();
-EXTERN_C DWORD SW3_GetSyscallNumber(DWORD FunctionHash);
-EXTERN_C PVOID SW3_GetSyscallAddress(DWORD FunctionHash);
-EXTERN_C PVOID internal_cleancall_wow64_gate(VOID);
-typedef struct _UNICODE_STRING
-{
-	USHORT Length;
-	USHORT MaximumLength;
-	PWSTR  Buffer;
+// Exposé publiquement (utilisé par les stubs ASM).
+EXTERN_C DWORD Qn3_ResolveSyscallId(DWORD Hash);
+EXTERN_C PVOID Qn3_ResolveSyscallStub(DWORD Hash);
+EXTERN_C PVOID Qn3_PickSyscallStub(DWORD Hash);
+
+// API interne (utilisée uniquement par syscalls.c).
+BOOL Qn3_BuildSyscallTable(void);
+
+typedef struct _UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PWSTR  Buffer;
 } UNICODE_STRING, *PUNICODE_STRING;
 
 #ifndef InitializeObjectAttributes
 #define InitializeObjectAttributes( p, n, a, r, s ) { \
-	(p)->Length = sizeof( OBJECT_ATTRIBUTES );        \
-	(p)->RootDirectory = r;                           \
-	(p)->Attributes = a;                              \
-	(p)->ObjectName = n;                              \
-	(p)->SecurityDescriptor = s;                      \
-	(p)->SecurityQualityOfService = NULL;             \
+    (p)->Length = sizeof( OBJECT_ATTRIBUTES );        \
+    (p)->RootDirectory = r;                           \
+    (p)->Attributes = a;                              \
+    (p)->ObjectName = n;                              \
+    (p)->SecurityDescriptor = s;                      \
+    (p)->SecurityQualityOfService = NULL;             \
 }
 #endif
 
-typedef struct _OBJECT_ATTRIBUTES
-{
-	ULONG           Length;
-	HANDLE          RootDirectory;
-	PUNICODE_STRING ObjectName;
-	ULONG           Attributes;
-	PVOID           SecurityDescriptor;
-	PVOID           SecurityQualityOfService;
+typedef struct _OBJECT_ATTRIBUTES {
+    ULONG Length;
+    HANDLE RootDirectory;
+    PUNICODE_STRING ObjectName;
+    ULONG Attributes;
+    PVOID SecurityDescriptor;
+    PVOID SecurityQualityOfService;
 } OBJECT_ATTRIBUTES, *POBJECT_ATTRIBUTES;
 
-typedef struct _CLIENT_ID
-{
-	HANDLE UniqueProcess;
-	HANDLE UniqueThread;
+typedef struct _CLIENT_ID {
+    HANDLE UniqueProcess;
+    HANDLE UniqueThread;
 } CLIENT_ID, *PCLIENT_ID;
 
-EXTERN_C NTSTATUS Sw3NtOpenProcess(
-	OUT PHANDLE ProcessHandle,
-	IN ACCESS_MASK DesiredAccess,
-	IN POBJECT_ATTRIBUTES ObjectAttributes,
-	IN PCLIENT_ID ClientId OPTIONAL);
+EXTERN_C NTSTATUS Qn3_NtOpenProcess(
+    OUT PHANDLE, IN ACCESS_MASK, IN POBJECT_ATTRIBUTES, IN PCLIENT_ID);
 
-EXTERN_C NTSTATUS Sw3NtOpenThread(
-	OUT PHANDLE ThreadHandle,
-	IN ACCESS_MASK DesiredAccess,
-	IN POBJECT_ATTRIBUTES ObjectAttributes,
-	IN PCLIENT_ID ClientId OPTIONAL);
+EXTERN_C NTSTATUS Qn3_NtOpenThread(
+    OUT PHANDLE, IN ACCESS_MASK, IN POBJECT_ATTRIBUTES, IN PCLIENT_ID);
 
-EXTERN_C NTSTATUS Sw3NtAllocateVirtualMemory(
-	IN HANDLE ProcessHandle,
-	IN OUT PVOID * BaseAddress,
-	IN ULONG ZeroBits,
-	IN OUT PSIZE_T RegionSize,
-	IN ULONG AllocationType,
-	IN ULONG Protect);
+EXTERN_C NTSTATUS Qn3_NtAllocateVirtualMemory(
+    IN HANDLE, IN OUT PVOID*, IN ULONG, IN OUT PSIZE_T, IN ULONG, IN ULONG);
 
-EXTERN_C NTSTATUS Sw3NtWriteVirtualMemory(
-	IN HANDLE ProcessHandle,
-	IN PVOID BaseAddress,
-	IN PVOID Buffer,
-	IN SIZE_T NumberOfBytesToWrite,
-	OUT PSIZE_T NumberOfBytesWritten OPTIONAL);
+EXTERN_C NTSTATUS Qn3_NtWriteVirtualMemory(
+    IN HANDLE, IN PVOID, IN PVOID, IN SIZE_T, OUT PSIZE_T);
 
-EXTERN_C NTSTATUS Sw3NtProtectVirtualMemory(
-	IN HANDLE ProcessHandle,
-	IN OUT PVOID * BaseAddress,
-	IN OUT PSIZE_T RegionSize,
-	IN ULONG NewProtect,
-	OUT PULONG OldProtect);
+EXTERN_C NTSTATUS Qn3_NtProtectVirtualMemory(
+    IN HANDLE, IN OUT PVOID*, IN OUT PSIZE_T, IN ULONG, OUT PULONG);
 
-EXTERN_C NTSTATUS Sw3NtSuspendThread(
-	IN HANDLE ThreadHandle,
-	OUT PULONG PreviousSuspendCount);
+EXTERN_C NTSTATUS Qn3_NtSuspendThread(IN HANDLE, OUT PULONG);
 
-EXTERN_C NTSTATUS Sw3NtResumeThread(
-	IN HANDLE ThreadHandle,
-	IN OUT PULONG PreviousSuspendCount OPTIONAL);
+EXTERN_C NTSTATUS Qn3_NtResumeThread(IN HANDLE, IN OUT PULONG);
 
-EXTERN_C NTSTATUS Sw3NtGetContextThread(
-	IN HANDLE ThreadHandle,
-	IN OUT PCONTEXT ThreadContext);
+EXTERN_C NTSTATUS Qn3_NtGetContextThread(IN HANDLE, IN OUT PCONTEXT);
 
-EXTERN_C NTSTATUS Sw3NtSetContextThread(
-	IN HANDLE ThreadHandle,
-	IN PCONTEXT Context);
+EXTERN_C NTSTATUS Qn3_NtSetContextThread(IN HANDLE, IN PCONTEXT);
 
-EXTERN_C NTSTATUS Sw3NtClose(
-	IN HANDLE Handle);
+EXTERN_C NTSTATUS Qn3_NtClose(IN HANDLE);
 
 #endif
